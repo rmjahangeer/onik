9d1a18b082f4fecf1ffb271470679ee7be31fabd
rmjahangir <callrmj@gmail.com>
2017-12-21 14:41:19 +0500

    dawd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -49,7 +49,7 @@
     /*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 
-    $('#mu-event-counter').countdown('2018/01/02').on('update.countdown', function (event) {
+    $('#mu-event-counter').countdown(/*'2018/01/02'*/$('#timer').val()).on('update.countdown', function (event) {
         //var $this = $(this).html(event.strftime(''
         //  + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
         //  + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
